d3ed61c1d4d43569ce8e42ef6a8edd8bab9d4a74
JingBh <jingbohao@yeah.net>
2021-04-17 00:16:02 +0800

    A perfect text scroll implementation.

--- pages/data.vue
+++ pages/data.vue
@@ -5,8 +5,10 @@
     <v-divider class="mt-6"/>
     <v-row class="mt-3">
       <v-col md="9" sm="12">
-        <v-card id="text" @scroll="scroll">
-          <v-card-text v-text="text" />
+        <v-card>
+          <v-card-text>
+            <pre id="text" ref="text" v-text="text" @scroll="throttledScroll" />
+          </v-card-text>
         </v-card>
       </v-col>
       <v-col md="3" sm="0">
@@ -24,31 +26,57 @@
 </template>
 
 <script>
-let space = "Lorem ipsum dolor sit amet, et eam possit similique, nam no elit feugiat argumentum. Id sea" +
+import { throttle } from 'lodash'
+
+const space = "Lorem ipsum dolor sit amet, et eam possit similique, nam no elit feugiat argumentum. Id sea" +
 " rebum facilisi signiferumque, aliquip nostrud ad nec. In alii delicata pri, doming principes sed no. " +
 "Ceteros mediocritatem ei eam. Soluta nonumes an mea, errem dissentias te qui. Ut justo deserunt has.\n\n\n"
 
 export default {
   name: "data1",
   data: () => ({
-    text: space +space+space+space+space+space+space+space+space,
+    text: '',
     count: 0
   }),
 
   methods: {
-    scroll(event) {
-      const ele = event.target
-      if (ele.scrollTop <= 100) {
-        this.text = this.count + space + this.text
-        this.count += 1
-
+    scroll() {
+      if (this.$refs.text.scrollTop <= 100) {
+        this.loadNextTrunk()
       }
+    },
+    loadNextTrunk(lastHeight) {
+      return new Promise((resolve) => {
+        const ele = this.$refs.text
+        lastHeight = lastHeight || ele.scrollHeight
+        const lastScroll = ele.scrollHeight - ele.scrollTop
+
+        const load = () => {
+          this.text = this.count + space + this.text
+          this.count += 1
+          this.$nextTick(() => {
+            ele.scrollTop = ele.scrollHeight - lastScroll
+            this.$nextTick(() => {
+              if (ele.scrollHeight - lastHeight <= ele.clientHeight) {
+                load()
+              } else resolve()
+            })
+          })
+        }
+        load()
+      })
     }
   },
 
   mounted() {
-    const ele = document.getElementById('text')
-    ele.scrollTop = ele.scrollHeight - ele.clientHeight
+    this.loadNextTrunk().then(() => {
+      const ele = this.$refs.text
+      ele.scrollTop = ele.scrollHeight - ele.clientHeight
+    })
+  },
+
+  created() {
+    this.throttledScroll = throttle(this.scroll, 100)
   }
 }
 </script>
@@ -56,12 +84,7 @@ export default {
 <style scoped>
 
 #text {
-  border-style: solid;
-  border-width: 1px;
-  border-radius: 15px;
-
-  overflow: scroll;
-
+  overflow-y: scroll;
   height: 50vh;
 }
 
